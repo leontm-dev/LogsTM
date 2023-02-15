db2066737a2aa0318ffd458bc338536de59f4dea
Leon <LAnneken09@gmail.com>
2023-02-15 20:43:40 +0100

    Commands added to the app

--- commands.js
+++ commands.js
@@ -5,9 +5,29 @@ const clientId= process.env["CLIENT_ID"]
 const { REST, SlashCommandBuilder, Routes, PermissionFlagsBits } = require("discord.js");
 //Commands
 const commands = [
-	
+	new SlashCommandBuilder()
+	.setName("log-channel")
+	.setDescription("Provide the channel, my logs should be send in")
+	.setDescriptionLocalizations({"de": "Lege den Kanal fest, in den die Logs gesendet werden sollen!"})
+	.addChannelOption(option => option.setName("channel").setNameLocalizations({"de": "kanal"}).setDescription("The log-channel").setDescriptionLocalizations({"de": "Der Log-Kanal"}).setRequired(true))
+	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 ].map(command => command.toJSON());
 const rest = new REST({ version: '10' }).setToken(token);
-rest.put(Routes.applicationCommand(clientId), { body: commands })
-	.then((data) => console.log(`Es wurde erfolgreich ${data.length} Commands ins System gespeist!`))
-	.catch(console.error);
+
+// Construct and prepare an instance of the REST module
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
